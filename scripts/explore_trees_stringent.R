c5981aac34822731890ed8604b73a086bd4728ce
Jason Stajich <jasonstajich.phd@gmail.com>
2022-08-02 12:53:00 -0700

    I think this is more of a test

--- scripts/explore_trees_stringent.R
+++ scripts/explore_trees_stringent.R
@@ -22,15 +22,15 @@ if (length(args) >=1 ) {
 }
 pepin=sprintf("%s/gene_trees/gene_trees.%s.summarize_%s.tsv",outdir,HMM,'PEP')
 
-#cdsin=sprintf("%s/gene_trees/gene_trees.%s.summarize_%s.tsv",outdir,HMM,'CDS')
+cdsin=sprintf("%s/gene_trees/gene_trees.%s.summarize_%s.tsv",outdir,HMM,'CDS')
 treeinfoAAall <- read_tsv(pepin)
-#treeinfoCDSall <- read_tsv(cdsin)
+treeinfoCDSall <- read_tsv(cdsin)
 
 max=max(treeinfoAAall$TAXCOUNT)
 cutoff_taxcount = max * 0.90
 cutoff_peplen = 400
 cutoff_treeness = 0.4
 treeinfoAA <- treeinfoAAall %>% filter(TAXCOUNT >= cutoff_taxcount & ALNLEN >= cutoff_peplen & treeness >= cutoff_treeness)
-#treeinfoCDS <- treeinfoCDSall %>% filter(TAXCOUNT >= cutoff_taxcount & ALNLEN >= cutoff_peplen*3 )
+treeinfoCDS <- treeinfoCDSall %>% filter(TAXCOUNT >= cutoff_taxcount & ALNLEN >= cutoff_peplen*3 & treeness >= cutoff_treeness)
 write_tsv(treeinfoAA,sprintf("%s/gene_trees/gene_trees.%s.filtered_tax90p_%s.tsv",outdir,HMM,'PEP'))
-#write_tsv(treeinfoCDS,sprintf("%s/gene_trees/gene_trees.%s.filtered_tax90p_%s.tsv",outdir,HMM,'CDS'))
+write_tsv(treeinfoCDS,sprintf("%s/gene_trees/gene_trees.%s.filtered_tax90p_%s.tsv",outdir,HMM,'CDS'))
